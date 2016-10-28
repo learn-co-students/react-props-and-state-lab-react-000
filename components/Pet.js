e0ce501aa2839d3ab629e2341e05344c18051b91
talum <tracy.a.lum@gmail.com>
2016-10-28 00:01:33 -0400

    wip

--- components/Pet.js
+++ components/Pet.js
@@ -3,28 +3,46 @@ const React = require('react');
 class Pet extends React.Component {
   constructor() {
     super();
+    this.onAdoptPet = this.onAdoptPet.bind(this);
+  }
+
+  onAdoptPet() {
+    this.props.onAdoptPet(this.props.pet.id);
   }
 
   render() {
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">Pet name: {this.props.pet.name} (gender: {this.props.pet.gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">Pet type: {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
+         if (this.props.isAdopted) {
           <button className="ui disabled button">Already adopted</button>
+         } else {
+          <button onClick={this.onAdoptPet} className="ui primary button">Adopt pet</button>
+         } 
         </div>
       </div>
     );
   }
+}
+
+Pet.propTypes = {
+  pet: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    type: React.PropTypes.string,
+    age: React.PropTypes.number,
+    weight: React.PropTypes.number,
+    gender: React.PropTypes.string
+  })
 }
 
 module.exports = Pet;
--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -3,10 +3,15 @@ const React = require('react');
 const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
+  isAdopted(pet) {
+    return this.props.adoptedPets.includes(pet);
+  }
+
   render() {
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        <code>&lt;Pet /&gt;</code> &nbsp;
+        this.props.pets.map(the_pet => {<Pet pet={the_pet} onAdoptPet={this.props.onAdoptPet} isAdopted={this.isAdopted(the_pet)} />})
       </div>
     );
   }
